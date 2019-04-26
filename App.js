c789b2abdd0555815aa17964609cc5a6290b673d
F1renDev <47530368+F1renDev@users.noreply.github.com>
2019-04-27 00:52:55 +0300

    React todo-list ready

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'tachyons';
+
+import TodoForm from './TodoForm';
+import './App.css';
+
+
+
+const App = () => {
+    return (
+            <div >
+                <TodoForm/>
+            </div>
+        );
+};
+
+export default App;
